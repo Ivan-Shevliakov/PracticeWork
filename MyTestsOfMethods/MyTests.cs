6e7bdf36a33d75c31f127c38bc26d2d64c97c9f3
Ivan <vanya.shevlyakov@bk.ru>
2025-01-09 23:47:15 +0700

    Добавление еще одного теста для проверки работы обработки данных с избыточностью или недостаточностью информации

--- MyTestsOfMethods/MyTests.cs
+++ MyTestsOfMethods/MyTests.cs
@@ -8,7 +8,7 @@ namespace MyTestsOfMethods
         [Fact]
         public void DataProcessingTestWithCorrectDataSeparatedByCommaOrBySpace()
         {
-            // Первый тест направлен на проверку обработки корреткных данных, записанных через пробел и точки с запятой  
+            // Первый тест направлен на проверку обработки корреткных данных, записанных через пробел или точки с запятой  
             // вводными данными теста будет строка "22; 04,02; 1032.2.2", ожидание, что выводом будет набор данных  (double , int, DateTime) равный (4.02, 22, 02.02.1032) 
             string x = "22; 04,02; 1032.2.2";
             (double, int, DateTime) expected = (4.02, 22, new DateTime(1032, 02, 02));
@@ -39,7 +39,7 @@ namespace MyTestsOfMethods
         [Fact]
         public void DataProcessingTestWithAnEmptyLine()
         {
-            // Третий тест направлен на проверку обработки пустой строки, но в различном порядке во входной строке
+            // Третий тест направлен на проверку обработки пустой строки
             // вводными данными теста будет пустая строка "" ожидание что выводом будет набор данных  (double , int, DateTime) равный (double.MinValue, int.MinValue, DateTime.MinValue) 
             string x = "";
             (double, int, DateTime) expected = (double.MinValue, int.MinValue, DateTime.MinValue);
@@ -71,6 +71,22 @@ namespace MyTestsOfMethods
             Assert.Equal(expected, actual);
             // вводными данными теста будет строка "sddaaaadadsczcasd", ожидание, что выводом будет набор данных  (double , int, DateTime) равный (double.MinValue, int.MinValue, DateTime.MinValue) 
             x = "sddaaaadadsczcasd";
+            expected = (double.MinValue, int.MinValue, DateTime.MinValue);
+            actual = converter.DataProcessing(x);
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void DataProcessingWithMoreOrLessInputDataThanNeeded()
+        {
+            // Пятый тест направлен на проверку обработки данных с большим или ментшим количеством входных данных
+            // вводными данными теста будет пустая строка "1 1" ожидание что выводом будет набор данных  (double , int, DateTime) равный (double.MinValue, int.MinValue, DateTime.MinValue) 
+            string x = "1 1";
+            (double, int, DateTime) expected = (double.MinValue, int.MinValue, DateTime.MinValue);
+            PIS.Converter converter = new PIS.Converter();
+            (double, int, DateTime) actual = converter.DataProcessing(x);
+            Assert.Equal(expected, actual);
+            // вводными данными теста будет пустая строка "1 1,2 2010.12.12 23" ожидание что выводом будет набор данных  (double , int, DateTime) равный (double.MinValue, int.MinValue, DateTime.MinValue) 
+            x = "1 1,2 2010.12.12 23";
             expected = (double.MinValue, int.MinValue, DateTime.MinValue);
             actual = converter.DataProcessing(x);
             Assert.Equal(expected, actual);
