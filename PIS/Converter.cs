6dd245a767d2787f150453a2089d9575bbad6088
Ivan <vanya.shevlyakov@bk.ru>
2025-01-10 15:32:46 +0700

    Разделения метода обработки данных

--- PIS/Converter.cs
+++ PIS/Converter.cs
@@ -8,12 +8,31 @@ namespace PIS
 {
     public class Converter
     {
-        public (double , int, DateTime) DataProcessing(string line)
+        public (double, int, DateTime) DataProcessing(string line)
         {
-            string[] strings;
+            string[] strings = LineSpliting(line);
             double Height = Double.MinValue;
             int PressureValue = int.MinValue;
             DateTime Date = DateTime.MinValue;
+            if (strings.Length != 3) { return (Height, PressureValue, Date); }
+            foreach (string s in strings)
+                if (int.TryParse(s, out int pressureValue)) 
+                {
+                    PressureValue = pressureValue; 
+                }
+                else if (s.Contains(",") & double.TryParse(s, out double height)) 
+                { 
+                    Height = height; 
+                }
+                else if (DateTime.TryParse(s, out DateTime date) & s.Split('.').Length == 3) 
+                { 
+                    Date = date; 
+                }
+            return (Height, PressureValue, Date);
+        }
+        private string[] LineSpliting(string line)
+        {
+            string[] strings;
             if (line.Contains(";"))
             {
                 strings = line.Split(';');
@@ -22,14 +41,8 @@ namespace PIS
             {
                 strings = line.Split(' ');
             }
-            if (strings.Length != 3) { return (Height, PressureValue, Date); }
-            foreach (string s in strings)
-            {
-                if (int.TryParse(s, out int pressureValue) ) { PressureValue = pressureValue; }
-                else if (s.Contains(",") & double.TryParse(s, out double height)) { Height = height; }
-                else if (DateTime.TryParse(s, out DateTime date) & s.Split('.').Length == 3) { Date = date; }
-            }
-            return (Height,PressureValue,Date);
+            return strings;
         }
-    }
+    } 
+
 }
